5ddb9c2c4a7807075bac0ff9f36f4ddc6cf8033a
netwelfare <didawangxiaofei@126.com>
2016-06-26 21:18:56 +0800

    研究日志的加载

--- org/apache/ibatis/logging/slf4j/Slf4jImpl.java
+++ org/apache/ibatis/logging/slf4j/Slf4jImpl.java
@@ -4,33 +4,43 @@ import org.apache.ibatis.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Slf4jImpl implements Log {
+public class Slf4jImpl implements Log
+{
 
-  private Logger log;
+	private Logger log;
 
-  public Slf4jImpl(Class<?> clazz) {
-    log = LoggerFactory.getLogger(clazz);
-  }
+	// 构造方法，利用了slf4j的类
+	public Slf4jImpl(Class<?> clazz)
+	{
+		System.out.println(log.isInfoEnabled());
+		log.info("hello {}", "world");
+		log = LoggerFactory.getLogger(clazz);
 
-  public boolean isDebugEnabled() {
-    return log.isDebugEnabled();
-  }
+	}
 
-  public void error(String s, Throwable e) {
-    log.error(s, e);
-  }
+	public boolean isDebugEnabled()
+	{
+		return log.isDebugEnabled();
+	}
 
-  public void error(String s) {
-    log.error(s);
-  }
+	public void error(String s, Throwable e)
+	{
+		log.error(s, e);
+	}
 
-  public void debug(String s) {
-    log.debug(s);
-  }
+	public void error(String s)
+	{
+		log.error(s);
+	}
 
-  public void warn(String s) {
-    log.warn(s);
-  }
+	public void debug(String s)
+	{
+		log.debug(s);
+	}
 
+	public void warn(String s)
+	{
+		log.warn(s);
+	}
 
 }
